2ff91de720c3989283636fd4dd5819a1d2032e03
Tin_DevPlus <tin.nguyen@devplus.edu.vn>
2022-08-26 10:31:24 +0700

    fix content 2

--- src/Page/AboutDev.js
+++ src/Page/AboutDev.js
@@ -2,9 +2,8 @@ import React from 'react'
 
 const AboutDev = () => {
   return (
-    <div>
-      <div style={{ margin: "20px 0px" }}>
-        <div className="container mt-3">
+    <div style={{margin: "0px 0px 0px 250px"}} className="content-wrapper">
+      <div className="container mt-3" style={{margin: "0px 0px 0px 20px"}}>
           <h2>Custom File</h2>
           <form action="/action_page.php">
             <input type="file" id="myFile" name="filename2" />
@@ -15,7 +14,6 @@ const AboutDev = () => {
           </form>
         </div>
       </div>
-    </div>
   )
 }
 
--- src/Page/RoadToBe.js
+++ src/Page/RoadToBe.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const RoadToBe = () => {
   return (
-    <div>RoadToBe</div>
+    <div style={{ margin: "0px 0px 0px 250px" }} className="content-wrapper">
+      <div className="container mt-3" style={{ margin: "0px 0px 0px 20px" }}>
+        <h2>Custom File</h2>
+        <form action="/action_page.php">
+          <input type="file" id="myFile" name="filename2" />
+          <div className="mt-3">
+            <button type="submit" className="btn btn-primary">Add</button>
+            <button type="button" class="btn btn-danger">Delete</button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
