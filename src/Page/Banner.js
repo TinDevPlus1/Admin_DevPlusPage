cbc27929b705bf5973241a9a85716ec3687a6d2a
Tin_DevPlus <tin.nguyen@devplus.edu.vn>
2022-08-26 10:22:40 +0700

    fix conten

--- src/Page/Banner.js
+++ src/Page/Banner.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div style={{margin: "20px 0px"}}>
-      <div className="container mt-3">
+    <div style={{margin: "0px 0px 0px 250px"}} className="content-wrapper">
+      <div className="container mt-3" style={{margin: "0px 0px 0px 20px"}}>
         <h2>Custom File</h2>
         <form action="/action_page.php">
           <input type="file" id="myFile" name="filename2" />
