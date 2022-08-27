28dc8ec4465272ba10fa672f67a3327355dfe369
HuyTran <apple@MacBook-Pro-cua-apple.local>
2022-08-27 09:31:15 +0700

    fix css 2

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -7,8 +7,8 @@ const SideNav = () => {
             <aside className="main-sidebar sidebar-dark-primary elevation-4">
                 {/* Brand Logo */}
                 <a href="index3.html" className="brand-link">
-                    <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8' }} />
-                    <span className="brand-text font-weight-light">AdminLTE 3</span>
+                    {/* <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8' }} /> */}
+                    <span className="brand-text font-weight-light">Dev Plus</span>
                 </a>
                 {/* Sidebar */}
                 <div className="sidebar">
@@ -18,7 +18,7 @@ const SideNav = () => {
                             <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
                         </div>
                         <div className="info">
-                            <a href="#" className="d-block">Alexander Pierce</a>
+                            <a href="#" className="d-block">Bat Man Team</a>
                         </div>
                     </div>
                     {/* SidebarSearch Form */}
